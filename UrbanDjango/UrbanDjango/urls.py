4b4cb8b3d0b66a17d0ede0d718296a13f8137dfd
BalalaikaBear <eightes@yandex.ru>
2024-12-02 17:00:31 +0300

    None

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import func, cls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('func/', func),
+    path('cls/', cls.as_view())
 ]
